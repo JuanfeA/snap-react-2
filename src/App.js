b6b05f4279a999939d16863d057ef709805c5ab3
JuanfeA <juanfe_a27@outlook.com>
2019-04-26 12:53:47 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Link, Switch, Route} from 'react-router-dom';
+import Home from './Home';
+import Post from './Post';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="logo" />
+          </Link>
+          <h1>Welcome To React</h1>
+          <Link to="/">Home</Link>
+          <Link to="/post">Post</Link>
+            
+        </header>
+       <Switch>
+       <Route exact path="/" component={Home}/>
+       <Route path="/post/" component={Post}/>
+       </Switch> 
+      </div>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App;
